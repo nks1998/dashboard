845b2ede7d906785d925e841156a06e0108d5624
Nihal Kumar Sharma <nihalkumarsharma@Nihals-MacBook-Air.local>
2024-10-06 19:30:35 +0530

    Add complete project

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,25 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { fetchCustomersPages, fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+import Pagination from "@/app/ui/invoices/pagination";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+  const customers = await fetchFilteredCustomers(query, currentPage);
+  const totalPages = await fetchCustomersPages(query)
+  return (
+    <div className="w-full">
+      <CustomersTable customers={customers} />
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
 }
